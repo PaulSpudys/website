56e986d8091e4d9c20c25aab33414f71dc0397c0
PaulSpudys <98715178+PaulSpudys@users.noreply.github.com>
2023-06-24 00:02:26 +0100

    updated

--- project/forms.py
+++ project/forms.py
@@ -7,6 +7,4 @@ from django.core.validators import EmailValidator
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.CharField(validators=[EmailValidator()])
-    
-    subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
